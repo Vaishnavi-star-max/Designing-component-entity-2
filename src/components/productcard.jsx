dab91cefca413ae1dd9b5008ce797db7460ed1f9
pallempativaishnavi@gmail.com <pallempativaishnavi@gmail.com>
2025-03-12 13:41:17 +0530

    completed

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
-// write product card here
+
+import React from 'react';
+import ViewProductButton from '../components/button';
+
+const Productcard = () => {
+   const productImage='https://m.media-amazon.com/images/I/510uTHyDqGL._AC_UF1000,1000_QL80_.jpg';
+   const productName='laptop';
+   const productPrice='Rs.83,000';
+
+  return (
+    <div className="product-card">
+      <img src={productImage} alt={productName} />
+      <h2>{productName}</h2>
+      <p>{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default Productcard;
